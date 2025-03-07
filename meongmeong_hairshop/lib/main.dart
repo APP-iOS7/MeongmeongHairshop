5d3e25f8a251a7d3240b2c4fcb3bbcb9f73e722c
BKbud <tsb07040@gmail.com>
2025-03-07 13:28:16 +0900

    회원가입 페이지 구현 & 파이어베이스 초기화 및 인증 구현

--- meongmeong_hairshop/lib/main.dart
+++ meongmeong_hairshop/lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:meongmeong_hairshop/providers/user_provider.dart';
 import 'package:provider/provider.dart';
 import 'routes.dart';
 import 'config/app_theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider(create: (context) => UserProvider())],
--- meongmeong_hairshop/lib/providers/user_provider.dart
+++ meongmeong_hairshop/lib/providers/user_provider.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/foundation.dart';
+import 'package:meongmeong_hairshop/models/pet.dart';
 import '../models/user.dart';
 
 class UserProvider with ChangeNotifier {
-  User _user = User(email: '', password: '', username: '', phoneNumber: '');
+  User _user = User(
+    email: '',
+    password: '',
+    username: '',
+    phoneNumber: '',
+    pet: Pet(name: '', breed: '', ageMonths: 0),
+  );
 
   String _confirmPassword = '';
   bool _passwordMatch = true;
+  bool _isPetAgeNum = true;
 
   User get user => _user;
   String get confirmPassword => _confirmPassword;
   bool get passwordMatch => _passwordMatch;
+  bool get isPetAgeNum => _isPetAgeNum;
 
   void updateEmail(String email) {
     _user.email = email;
@@ -28,6 +37,34 @@ class UserProvider with ChangeNotifier {
     notifyListeners();
   }
 
+  void updateUsername(String username) {
+    _user.username = username;
+    notifyListeners();
+  }
+
+  void updatePhoneNumber(String phoneNumber) {
+    _user.phoneNumber = phoneNumber;
+    notifyListeners();
+  }
+
+  void updatePetName(String petName) {
+    _user.pet.name = petName;
+    notifyListeners();
+  }
+
+  void updatePetBreed(String petBreed) {
+    _user.pet.breed = petBreed;
+    notifyListeners();
+  }
+
+  void updatePetAgeMonth(String petAgeMonth) {
+    _checkAgeValue(petAgeMonth);
+    if (_isPetAgeNum) {
+      _user.pet.ageMonths = int.parse(petAgeMonth);
+    }
+    notifyListeners();
+  }
+
   // 비밀번호가 일치하는지 확인하는 함수
   void _checkPasswordMatch(String password, String confirmPassword) {
     if (password == confirmPassword) {
@@ -37,13 +74,12 @@ class UserProvider with ChangeNotifier {
     }
   }
 
-  void updateUsername(String username) {
-    _user.username = username;
-    notifyListeners();
-  }
-
-  void updatePhoneNumber(String phoneNumber) {
-    _user.phoneNumber = phoneNumber;
-    notifyListeners();
+  void _checkAgeValue(String input) {
+    final age = int.tryParse(input);
+    if (age == null || age <= 0) {
+      _isPetAgeNum = false;
+    } else {
+      _isPetAgeNum = true;
+    }
   }
 }
